387b8734dbcb77f2fb74910af1a26139443f2f3b
Ronoh Levy Kipkoech <kipkoechlevy237@gmail.com>
2025-12-12 19:34:47 +0300

    second commit

--- exercises.py
+++ exercises.py
@@ -18,5 +18,17 @@
 # pounds = kg * 2.20462262
 # print("Your wight is: ", pounds, "pounds")
 
+correct_password = 12345
+user_password = float(input("Enter the password: "))
+trial = 1
+  
+while user_password != correct_password and trial < 5:
+    print("Incorrect password! try again.")  
+    trial += 1
+    user_password = float(input("Enter the password: "))
+if user_password == correct_password:
+    print("Your'e logged in to the system.")
+else:
+    print("Your'e kicked off from the system.")
 
 
